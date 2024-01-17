2c6f77b5d0e56de291c6e4863108baa2690b51a4
cyberspacelee <xingcanli132@163.com>
2024-01-18 01:25:33 +0800

    main settings

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,6 +12,12 @@ function createWindow(): void {
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     center: true,
+    title: 'Note App',
+    frame: false,
+    vibrancy: 'under-window',
+    visualEffectState: 'active',
+    titleBarStyle: 'hidden',
+    trafficLightPosition: {"x": 15, "y": 10},
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
